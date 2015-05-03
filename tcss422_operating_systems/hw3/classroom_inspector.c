30a9662ce485841f0f9a9ff24bbc43b9307e3c44
BuildTools <jessebannon@live.com>
2015-05-02 19:47:20 -0700

    error checks, completed

--- tcss422_operating_systems/hw3/classroom_inspector.c
+++ tcss422_operating_systems/hw3/classroom_inspector.c
@@ -41,12 +41,8 @@ typedef struct _parse_info {
     char ** URLs, ** html_pages;
     unsigned int bounds[2];
     time_schedule * times;
+    int rv;
 } parse_info;
-
-unsigned int getURLS(const char * building,
-            const char * room,
-            const char * time_schedule_url,
-            char * ** webpageURLs);
 
 /*
 ===============================================================================
@@ -64,7 +60,7 @@ void inspect_classroom(const char * building,
 
 
 /* Parsing Functions */
-unsigned int getURLS(const char * building,
+int getURLS(const char * building,
             const char * room,
             const char * time_schedule_url,
             char * ** webpageURLs);
@@ -73,18 +69,18 @@ void * retrieve_page(void * params);
 
 void * inspect_page(void * params);
 
-void parse_page(const char * building,
-                const char * room,
-                char * classpage,
-                time_schedule * schedule);
+int parse_page(const char * building,
+               const char * room,
+               char * classpage,
+               time_schedule * schedule);
 
-void add_class(time_schedule * schedule,
-               char * webpage,
-               unsigned int class_idx,
-               unsigned int times_idx,
-               unsigned int class_len,
-               unsigned int times_len,
-               day d);
+int add_class(time_schedule * schedule,
+              char * webpage,
+              unsigned int class_idx,
+              unsigned int times_idx,
+              unsigned int class_len,
+              unsigned int times_len,
+              day d);
 
 
 /* Printing Functions */
@@ -101,14 +97,14 @@ void print_schedule(time_schedule * schedule);
 
 
 /* Utility Functions */
-void initialize_schedule(time_schedule ** schedule,
+int initialize_schedule(time_schedule ** schedule,
                          const char * building,
                          const char * room);
 
 unsigned int get_start_time(char * time);
 
-unsigned int increaseAllocation(char ** array,
-                                unsigned int * size);
+int increaseAllocation(char * ** array,
+                       unsigned int * size);
 
 int strcmpin(char * str1,
              const char * str2,
@@ -135,6 +131,7 @@ void inspect_classroom(const char * building,
 {
     char * content, * walker, ** webpages, ** webpageURLs;
     unsigned int len, idx, link_idx, k, webpage_size, url_size;
+    int rv;
     time_schedule * schedule;
 
     idx = (retrieval_threads > analysis_threads)
@@ -143,19 +140,44 @@ void inspect_classroom(const char * building,
     parse_info r_info[idx];
     
     url_size = getURLS(building, room, time_schedule_url, &webpageURLs);
+    if (url_size < 0) {
+        printf("an error occured.\n");
+        return;
+    }
+
     webpages = malloc(url_size * sizeof(char *));
-    initialize_schedule(&schedule, building, room);
+
+    if (webpages == NULL) {
+        perror("malloc error: ");
+        return;
+    }
+
+    rv = initialize_schedule(&schedule, building, room);
+    if (rv != 0) {
+        perror("initialize_schedule error: ");
+        return;
+    }
 
     for (idx = 0; idx < retrieval_threads; idx++) {
         r_info[idx].URLs = webpageURLs;
         r_info[idx].html_pages = webpages;
         r_info[idx].bounds[0] = ((url_size/retrieval_threads)*idx);
         r_info[idx].bounds[1] = ((url_size/retrieval_threads)*(idx+1));
-        pthread_create(&r_threads[idx], NULL, &retrieve_page, &r_info[idx]);
+        r_info[idx].rv = 0;
+        rv = pthread_create(&r_threads[idx], NULL, &retrieve_page, &r_info[idx]);
+        if (rv != 0) {
+            perror("pthread_create error: ");
+            return;
+        }
     }
 
-    for (idx = 0; idx < retrieval_threads; idx++)
+    for (idx = 0; idx < retrieval_threads; idx++) {
         pthread_join(r_threads[idx], NULL);
+        if (r_info[idx].rv < 0) {
+            printf("an error occured.\n");
+            return;
+        }
+    }
 
     for (idx = 0; idx < analysis_threads; idx++) {
         r_info[idx].URLs = webpageURLs;
@@ -163,11 +185,21 @@ void inspect_classroom(const char * building,
         r_info[idx].bounds[0] = ((url_size/analysis_threads)*idx);
         r_info[idx].bounds[1] = ((url_size/analysis_threads)*(idx+1));
         r_info[idx].times = schedule;
-        pthread_create(&r_threads[idx], NULL, &inspect_page, &r_info[idx]);
+        r_info[idx].rv = 0;
+        rv = pthread_create(&r_threads[idx], NULL, &inspect_page, &r_info[idx]);
+        if (rv != 0) {
+            perror("pthread_create error: ");
+            return;
+        }
     }
 
-    for (idx = 0; idx < analysis_threads; idx++)
+    for (idx = 0; idx < analysis_threads; idx++) {
         pthread_join(r_threads[idx], NULL);
+        if (r_info[idx].rv < 0) {
+            printf("an error occured.\n");
+            return;
+        }
+    }
 
     sort_schedule(schedule);
     print_schedule(schedule);
@@ -179,19 +211,16 @@ void inspect_classroom(const char * building,
  * Doubles char pointer array allocation. Used for increasing class     *
  * string pointers.                                                     *
  * ==================================================================== */
-unsigned int increaseAllocation(char ** array,
-                                unsigned int * size)
+int increaseAllocation(char * ** array,
+                       unsigned int * size)
 {
-    unsigned int newsize = *size * 2;
-    char ** newArray;
-    newArray = realloc(array, newsize*sizeof(char*));
-    if (newArray == NULL) {
-        printf("Memory reallocation failed\n");
-        return 0;
+    *array = realloc(*array, *size * 2 * sizeof(char*));
+    if (array == NULL) {
+        perror("realloc error: ");
+        return -1;
     }
-    *size = newsize;
-    array = newArray;
-    return 1;
+    *size *= 2;
+    return 0;
 }
 
 /* ==================================================================== *
@@ -218,19 +247,30 @@ int strcmpin(char * str1,
  * Initializes a schedule pointer. Allocates 12 pointers to a single    *
  * day.                                                                 *
  * ==================================================================== */
-void initialize_schedule(time_schedule ** schedule,
+int initialize_schedule(time_schedule ** schedule,
                          const char * building,
                          const char * room)
 {
-    (*schedule) = malloc(sizeof(time_schedule)); 
+    int rv;
+    (*schedule) = malloc(sizeof(time_schedule));
+    if (*schedule == NULL) {
+        perror("malloc error: ");
+        return 1;
+    }
+
     (*schedule)->building = building;
     (*schedule)->room = room;
     int i;
     for (i = 0; i < 5; i++) {
         (*schedule)->idx[i] = 0;
         (*schedule)->days[i] = malloc(12 * sizeof(char *));
-        pthread_spin_init(&(*schedule)->locks[i], 0);
-    }   
+        rv = pthread_spin_init(&(*schedule)->locks[i], 0);
+        if (rv != 0) {
+            perror("pthread_spin_init error: ");
+            return 1;
+        }
+    }
+    return 0;   
 }
 
 /* ==================================================================== *
@@ -253,7 +293,7 @@ unsigned int get_start_time(char * time)
         hour = (10 + (time[1] - '0'));
         minute = (10*(time[2] - '0') + (time[3] - '0'));
     }
-    if (hour < 6)
+    if (hour < 5)
         hour += 12;
     while (digits < 10) {
         if (time[digits++] == 'P')
@@ -308,15 +348,15 @@ void print_day(char ** day_sched,
     int i;
     char * abbr;
     switch(d) {
-    case MON: abbr = "M"; break;
-    case TUE: abbr = "T"; break;
-    case WED: abbr = "W"; break;
-    case THU: abbr = "Th"; break;
-    case FRI: abbr = "F"; break;
+    case MON: abbr = "M \0"; break;
+    case TUE: abbr = "T \0"; break;
+    case WED: abbr = "W \0"; break;
+    case THU: abbr = "Th\0"; break;
+    case FRI: abbr = "F \0"; break;
     default: perror("Invalid Day"); return;
     }
     if (size)
-        printf("%d  %s\n", d, day_sched[0]);
+        printf("%s %s\n", abbr, day_sched[0]);
     for (i = 1; i < size; i++)
         printf("   %s\n", day_sched[i]);
 }
@@ -338,7 +378,7 @@ void print_schedule(time_schedule * schedule) {
  * add_class:                                                           *
  * Adds a class from the webpage into the time_schedule.                *
  * ==================================================================== */
-void add_class(time_schedule * schedule,
+int add_class(time_schedule * schedule,
                char * webpage,
                unsigned int class_idx,
                unsigned int times_idx,
@@ -348,21 +388,31 @@ void add_class(time_schedule * schedule,
 {
     char * class;
     unsigned int i, * idx;
+    int rv;
 
     if (d >= DNE) {
         printf("[ERROR] Invalid day\n");
-        return;
+        return 1;
     }
 
     class = malloc(24 * sizeof(char));
-    sprintf(class, "%-12.*s %.*s", times_len,
-                                   webpage+times_idx,
-                                   class_len,
-                                   webpage+class_idx);
+    if (class == NULL) {
+        perror("malloc error :");
+        return 1;
+    }
+
+    rv = sprintf(class, "%-12.*s %.*s", times_len,
+                                        webpage+times_idx,
+                                        class_len,
+                                        webpage+class_idx);
+    if (rv < 0) {
+        perror("sprintf error: ");
+        return 1;
+    }
 
     for (i = 0; i < schedule->idx[d]; i++) {
         if (strcmp(class, schedule->days[d][i]) == 0)
-            return;
+            return 0;
     }
 
     /* Critical Section */
@@ -371,6 +421,7 @@ void add_class(time_schedule * schedule,
     schedule->days[d][(*idx)++] = class;
     pthread_spin_unlock(&schedule->locks[d]);
     /* End of Critical Section */
+    return 0;
 }              
 
 /* ==================================================================== *
@@ -378,13 +429,14 @@ void add_class(time_schedule * schedule,
  * Parses a major's class list and adds any times for the particular    *
  * building and room to the time_schedule.                              *
  * ==================================================================== */
-void parse_page(const char * building,
-                const char * room,
-                char * classpage,
-                time_schedule * schedule)
+int parse_page(const char * building,
+               const char * room,
+               char * classpage,
+               time_schedule * schedule)
 {
     unsigned int i, p_len, b_len, r_len, t_len, n_len, d_len;
     unsigned int t_idx, n_idx, d_idx;
+    int rv;
     char * walker;
     day temp_day;
     p_len = strlen(classpage);
@@ -431,19 +483,24 @@ void parse_page(const char * building,
                     else temp_day = DNE;
                     
                     if (temp_day != DNE)
-                    add_class(schedule, 
-                              classpage, 
-                              n_idx,
-                              t_idx,
-                              n_len,
-                              t_len,
-                              temp_day);
+                    rv = add_class(schedule, 
+                                   classpage, 
+                                   n_idx,
+                                   t_idx,
+                                   n_len,
+                                   t_len,
+                                   temp_day);
+                    if (rv != 0) {
+                        perror("add_class error: ");
+                        return -1;
+                    }
                     ++i;
                 }
-                i += (d_len + t_len + b_len + r_len + 50);
+                i += (d_len + t_len + b_len + r_len + 20);
             }
         }
     }
+    return 0;
 }
 
 /* ==================================================================== *
@@ -454,12 +511,17 @@ void parse_page(const char * building,
 void * inspect_page(void * params)
 {
     parse_info* r_info = params;
-    int i;
+    int i, rv;
     for (i = r_info->bounds[0]; i < r_info->bounds[1]; i++) {
-        parse_page(r_info->times->building,
-                   r_info->times->room,
-                   r_info->html_pages[i],
-                   r_info->times);
+        rv = parse_page(r_info->times->building,
+                        r_info->times->room,
+                        r_info->html_pages[i],
+                        r_info->times);
+        if (rv < 0) {
+            printf("an error occured.\n");
+            r_info->rv = -1;
+            return;
+        }
     }   
 }
 
@@ -475,25 +537,36 @@ void * retrieve_page(void * params)
 
     for (i = r_info->bounds[0]; i < r_info->bounds[1]; i++) {
         r_info->html_pages[i] = getContent(r_info->URLs[i]);
-        if (r_info->html_pages[i] == NULL); //pthread error exit
+        if (r_info->html_pages[i] == NULL) {
+            printf("error: could not load webpage.\n");
+            r_info->rv = -1;
+            return;
+        }
     }
 }
 
 
-unsigned int getURLS(const char * building,
+int getURLS(const char * building,
             const char * room,
             const char * time_schedule_url,
             char * ** webpageURLs)
 {
     unsigned int len, idx, link_idx, webpage_count, url_len, url_size;
     char *webpage;
+    int rv;
 
-    if ((webpage = getContent(time_schedule_url)) == NULL)
-        printf("error here\n");
+    if ((webpage = getContent(time_schedule_url)) == NULL) {
+        printf("error: could not load webpage.\n");
+        return -1;
+    }
 
     url_size = 64;
     url_len = strlen(time_schedule_url);
     *webpageURLs = malloc(url_size * sizeof(char*));
+    if (*webpageURLs == NULL) {
+        perror("malloc error: ");
+        return -1;
+    }
     len = strlen(webpage);
 
     webpage_count = 0;
@@ -501,14 +574,22 @@ unsigned int getURLS(const char * building,
     for (idx = 0; idx < len; idx++) {
         if (strcmpin(webpage+idx, "<li><a href=", 12) == 0) {
             if (webpage_count == url_size) {
-                printf("increaseed! Test this!!!");
-                increaseAllocation(*webpageURLs, &url_size);
+                rv = increaseAllocation(webpageURLs, &url_size);
+                if (rv < 0) {
+                    printf("an error occured.\n");
+                    return -1;
+                }
             }
 
             idx += 12;
             link_idx = url_len;
 
             char * tempLink = malloc((url_len + 12) * sizeof(char));
+            if (tempLink == NULL) {
+                perror("malloc error: ");
+                return -1;
+            }
+
             strcpy(tempLink, time_schedule_url);
           
             while (webpage[idx] != '>')
