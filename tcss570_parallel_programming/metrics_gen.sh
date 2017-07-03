366c5f0a52a5e6eac2989e9fcc07ddf761f27dac
Jesse Bannon <jmbannon@uw.edu>
2017-07-02 21:12:48 -0700

    Update metrics generation bash file

--- tcss570_parallel_programming/metrics_gen.sh
+++ tcss570_parallel_programming/metrics_gen.sh
@@ -7,24 +7,24 @@ STRASSEN_EXEC=./multiply_strassen
 MATRICES[0]="data/nnc1374.mtx"
 MATRICES[1]="data/lns_3937.mtx"
 
-# for i in 0 1; do
-#     sleep 1
-#     $STHREAD_EXEC ${MATRICES[$i]} ${MATRICES[$i]}
-# done
-
-# for i in 0 1; do
-#     sleep 1
-#     for threads in 6 12; do
-#         sleep 1
-#         $PTHREAD_EXEC ${MATRICES[$i]} ${MATRICES[$i]} $threads
-#     done
-# done
+for i in 0 1; do
+    sleep 1
+    $STHREAD_EXEC ${MATRICES[$i]} ${MATRICES[$i]}
+done
 
 for i in 0 1; do
     sleep 1
-    for threads in 1 2 4 8 16; do
+    for threads in 1 2 4 6 8 12 16; do
         sleep 1
-        for min_size in 1024; do
+        $PTHREAD_EXEC ${MATRICES[$i]} ${MATRICES[$i]} $threads
+    done
+done
+
+for i in 0 1; do
+    sleep 1
+    for threads in 1 2 4 6 8 12 16; do
+        sleep 1
+        for min_size in 32 64 128 256 512 1024; do
             sleep 1
             $STRASSEN_EXEC ${MATRICES[$i]} ${MATRICES[$i]} $threads $min_size
         done
