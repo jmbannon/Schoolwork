79c46bc2bcf6f341612689fe55ef38c899c5fe3c
Jesse Bannon <jmbannon@uw.edu>
2018-03-04 16:52:29 -0800

    Update project with sbt build and jar

---
+++ tcss543_advanced_algorithms/GraphColoring/build.sbt
@@ -0,0 +1,15 @@
+name := "TCSS543"
+version := "1.0"
+scalaVersion := "2.12.3"
+mainClass in assembly := some("dsatur.Main")
+assemblyJarName := "TCSS543.jar"
+
+val meta = """META.INF(.)*""".r
+assemblyMergeStrategy in assembly := {
+  case PathList("javax", "servlet", xs @ _*) => MergeStrategy.first
+  case PathList(ps @ _*) if ps.last endsWith ".html" => MergeStrategy.first
+  case n if n.startsWith("reference.conf") => MergeStrategy.concat
+  case n if n.endsWith(".conf") => MergeStrategy.concat
+  case meta(_) => MergeStrategy.discard
+  case x => MergeStrategy.first
+}
---
+++ tcss543_advanced_algorithms/GraphColoring/src/main/scala/dsatur/Main.scala
@@ -0,0 +1,51 @@
+package dsatur
+
+object Main extends App {
+
+  /** Toggles to compute average of runtime or number of colors. True for runtime. False for number of colors. */
+  private val RUNTIME_NUMCOLOR_TOGGLE = true
+
+//  def main(args: Array[String]): Unit = {
+    val nrTrials = 100
+
+    // Prints density percentage at top
+    print("    ")
+    for (densityInt <- 5 to 95 by 5) {
+      print(f"${densityInt.toDouble}%2.3f,")
+    }
+    println()
+
+    for (nrVertices <- 100 to 1000 by 100) {
+      print(s"$nrVertices")
+      for (densityInt <- 5 to 95 by 5) {
+        var elapsedTimeSum = 0.0
+        var numColorsSum = 0L
+
+        for (_ <- 0 until nrTrials) {
+          val density = densityInt.toDouble / 100.0
+          val graph = Graph.initialize(nrVertices, density)
+          val t0 = System.currentTimeMillis()
+          graph.color()
+          val timeElapsedSeconds = (System.currentTimeMillis() - t0).toDouble / 1000.0
+          elapsedTimeSum += timeElapsedSeconds
+          numColorsSum += graph.colorCount()
+
+          if (!graph.verify()) {
+            println(s"ERROR: Invalid output. $nrVertices vertices; $density density")
+            System.exit(1)
+          }
+        }
+
+        val aveElapsedTime = elapsedTimeSum / nrTrials.toDouble
+        val aveNumColors = numColorsSum.toDouble / nrTrials.toDouble
+
+        if (RUNTIME_NUMCOLOR_TOGGLE)
+          print(f",$aveElapsedTime%1.4f")
+        else
+          print(f",$aveNumColors%1.4f")
+
+      }
+      println()
+    }
+//  }
+}
